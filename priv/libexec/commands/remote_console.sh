f64bf92c2c5b58c4e51c353a0a49533e2fa7afff
Dong Minh Thai <thai.dm279@gmail.com>
2024-04-29 11:21:34 +0700

    Update remote_console.sh

--- priv/libexec/commands/remote_console.sh
+++ priv/libexec/commands/remote_console.sh
@@ -23,7 +23,7 @@ if [ ! -z "$USE_ERL_SHELL" ]; then
         -remsh "$NAME" \
         -setcookie "$COOKIE"
 else
-    iex --erl "-hidden -kernel net_ticktime $TICKTIME" \
+    exec iex --erl "-hidden -kernel net_ticktime $TICKTIME" \
         --logger-sasl-reports false \
         -"$NAME_TYPE" "$id" \
         --cookie "$COOKIE" \
